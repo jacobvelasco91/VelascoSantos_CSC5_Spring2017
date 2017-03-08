9d7c7b2da01e1915ab36a9d8d999557e0b8318a1
iMac <jacobvelasco91@gmail.com>
2017-03-08 14:11:20 -0800

    Changes to milGal on assignment 2

--- homework/Assignment_2/Gaddis_8thed_Ch3_Prob1_MilGal/main.cpp
+++ homework/Assignment_2/Gaddis_8thed_Ch3_Prob1_MilGal/main.cpp
@@ -19,13 +19,13 @@ using namespace std; //Name-space under which system libraries exist
 //Execution begins here
 int main(int argc, char** argv) {
     //Declare variables
-    long milTnk,tnkGal,milPrgl; //Miles per tank, How many gallons a tank holds
+    float milTnk,tnkGal,milPrgl; //Miles per tank, How many gallons a tank holds
                                 // and miles per gallon
     
     //Initialize variables
     
     //Input data
-    cout<<"Enter the number of gallons of gas you car can hold\n";
+    cout<<"Enter the number of gallons of gas your car can hold\n";
     cin>>tnkGal;
     cout<<"Enter the number of miles it can drive on a full tank\n";
     cin>>milTnk;
@@ -36,7 +36,9 @@ int main(int argc, char** argv) {
     milPrgl=milTnk/tnkGal;
 
     //Output the transformed data
-    cout<<"Your car gets "<<milPrgl<<" MPG "<<endl;
+    cout<<fixed<<setprecision(1)<<showpoint;
+    cout<<"Your car got "<<milPrgl<<" MPG "<<endl;
+    cout<<"which is not too bad!"<<endl;
    
    
     
