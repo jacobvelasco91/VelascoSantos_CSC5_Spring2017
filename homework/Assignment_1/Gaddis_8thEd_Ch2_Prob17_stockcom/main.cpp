45bb745c4af265bdfb4378657dad14408fc554b5
iMac <jacobvelasco91@gmail.com>
2017-03-08 13:54:44 -0800

    Assignment 1 folder, test push.

---
+++ homework/Assignment_1/Gaddis_8thEd_Ch2_Prob17_stockcom/main.cpp
@@ -0,0 +1,46 @@
+/* 
+  File:   main.cpp
+  Author: Santos Velasco
+  Created on February 24, 2017, 12:30 PM
+  Purpose:  Problem 17 - stock commission 
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Such as PI, Vc, -> Math/Science values
+//as well as conversions from system of units to 
+//another
+const unsigned char PERCENT=100;//Conversion to Percent
+
+//Function Prototypes
+
+//Executable code begins here!!!
+int main(int argc, char** argv) {
+    //Declare and Initialize Variables
+    unsigned short stkPrce=35;  //Units of $'s/share
+    unsigned short nShares=750;//Number of Shares
+    unsigned char comishn=2;   //Percent Commission
+    unsigned short stkPaid,comPaid,totPaid;
+    
+    //Process by mapping inputs to outputs
+    stkPaid=stkPrce*nShares;          //Amount Paid for the Stock Purchase $'s
+    comPaid=stkPaid*comishn/PERCENT;  //Commission on Stock Purchase $'s
+    totPaid=stkPaid+comPaid;          //Total Paid $'s
+    
+    //Output values
+    cout<<"Stock Price/Share= $   "<<(stkPrce)<<"/share"<<endl;
+    cout<<"Number of Shares =    "<<nShares<<"shares"<<endl;
+    cout<<"Commission              "<<static_cast<int>(comishn)<<"%"<<endl;
+    cout<<"Stock Price Paid = $"<<stkPaid<<endl;
+    cout<<"Commission Paid  = $  "<<comPaid<<endl;
+    cout<<"Total Paid       = $"<<totPaid<<endl;
+    
+
+    //Exit stage right!
+    return 0;
+}
