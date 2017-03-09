9e8faadef2444a1f0c04099df52ec1d133202d53
rcc <rcc>
2017-03-09 14:57:05 -0800

    Signed-off-by: rcc <rcc>

--- homework/Assignment_2/Gaddis_8thed_Ch3_Prob9_calories/main.cpp
+++ homework/Assignment_2/Gaddis_8thed_Ch3_Prob9_calories/main.cpp
@@ -1,8 +1,8 @@
 /* 
  * File:   main.cpp
  * Author: Santos Velasco
- * Created on March 09, 2017, 1:37 PM
- * Purpose:  Template
+ * Created on March 09, 2017, 2:12 PM
+ * Purpose:  calculate how many calories a snack holds.
  */
 
 //System Libraries
@@ -19,19 +19,28 @@ using namespace std; //Name-space under which system libraries exist
 //Execution begins here
 int main(int argc, char** argv) {
     //Declare variables
+    int bgCks=30;
+    int srvng=10;
+    float echsrv=300;
+    long ckAte,calPck,calAte;
   
     
     //Initialize variables
-    
+    calPck=(echsrv*srvng)/bgCks;
+            
     //Input data
+    cout<<"How many cookies did you actually eat? ";cin>>ckAte;
    
-          
-    
     //Map inputs to outputs or process the data
- 
+    calAte=calPck*calAte;
+    
+    if (ckAte>=bgCks)
+        cout<<"You ate the whole bag which is 3000 calories!!"<<endl;
+   
+    
    
     //Output the transformed data
-   
+    cout<<"you consumed "<<calAte<<" calories."<<endl;
    
     
     //Exit stage right!
