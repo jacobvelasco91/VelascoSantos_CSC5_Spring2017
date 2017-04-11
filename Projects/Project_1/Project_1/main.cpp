2bb1330935691d491b09bbb15fc1c3f752ead081
iMac <jacobvelasco91@gmail.com>
2017-04-11 00:18:00 -0700

    Project 1 started

---
+++ Projects/Project_1/Project_1/main.cpp
@@ -0,0 +1,110 @@
+/* 
+ * File:   main.cpp
+ * Author: Santos Velasco
+ * Created on April 10, 2017, 5:20 PM
+ * Purpose:  Project 1 
+ *           A game similar to Jeopardy.
+ */
+
+//System Libraries
+#include <iostream>  //Input - Output Library
+#include <iomanip>   // Formatting
+#include <cmath>     //math operators
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare variables
+    char choice;
+    
+   
+   
+    
+  
+    
+  
+    
+    //Welcoming text and Name of Game.
+    cout<<"                     -----------------------------------"<<endl;
+    cout<<"                     - Welcome to Guess the word Game! -"<<endl;
+    cout<<" -------------------------------------------------------------"
+        <<"--------------"<<endl;    
+    
+    //Explanation of the game
+    cout<<" - Similar to Jeopardy, this game displays 3 columns which are "
+        <<"categories. -\n";
+    cout<<" - In these categories there are 3 questions which are rated easy," 
+        <<" medium  -"<<endl;
+    cout<<" - or hard. To play you will have to choose a category,then choose "
+        <<"a level -"<<endl;
+    cout<<" - of difficulty. Points will be awarded accordingly for each "
+        <<"question you -"<<endl; 
+    cout<<" - get correct. Have fun!                                         "
+            <<"         -"<<endl;
+    cout<<" -------------------------------------------------------------"
+        <<"--------------"<<endl;   
+    cout<<endl;
+    
+    //Instructions on how to play the game
+    cout<<"To choose a category enter the character 'M' for movies, 'A' for "
+        <<"animals, 'H' for history.\n";   
+    cout<<"To choose a level of difficulty enter 'easy', 'medium', or 'hard'\n";
+    cout<<endl;
+    //Display Game table.
+    cout<<"  ______________________________________________________________ \n";
+    cout<<" |      MOVIES       |       ANIMALS       |       HISTORY      |\n";
+    cout<<" |===================|=====================|====================|\n";
+    cout<<" |                   |                     |                    |\n";
+    cout<<" |       Easy        |        Easy         |        Easy        |\n";
+    cout<<" |                   |                     |                    |\n";
+    cout<<" |*******************|*********************|********************|\n";
+    cout<<" |                   |                     |                    |\n";
+    cout<<" |      Medium       |       Medium        |       Medium       |\n";
+    cout<<" |                   |                     |                    |\n";
+    cout<<" |*******************|*********************|********************|\n";
+    cout<<" |                   |                     |                    |\n";
+    cout<<" |       Easy        |        Easy         |        Easy        |\n";
+    cout<<" |                   |                     |                    |\n";
+    cout<<" |*******************|*********************|********************|\n";
+    
+    //Have the user enter category and level of difficulty
+    
+   
+    
+   
+    
+    
+    
+           
+    
+    
+    
+    
+    //Input data
+    
+    
+    
+    
+   
+    
+    
+   
+   
+ 
+    //Map inputs to outputs or process the data   
+    
+       
+   
+    //Output the transformed data
+    
+    
+    //Exit stage right!
+    return 0;
+}
+
