4c222f12803b0a8b000d45a302829725fc2299f8
iMac <svelasco5@student.rccd.edu>
2017-12-19 15:00:13 -0800

    checking out old projects

--- C++_Template/main.cpp
+++ C++_Template/main.cpp
@@ -20,25 +20,33 @@ using namespace std; //Name-space under which system libraries exist
 //Execution begins here
 int main(int argc, char** argv) {
     //Declare variables
+    int num1;
+    int num2;
+    int product = 0;
    
     
   
     
     //Initialize variables
-    
+    cout<<"Please Enter A Number to multiply them."<<endl;
     
     //Input data
+    cin>>num1;
+    cout<<"Enter next number : "; cin>>num2;
+    
+    
   
  
     //Map inputs to outputs or process the data  
-    
+    product = num1 * num2;
  
        
    
     //Output the transformed data
-    
+    cout<<"Your multiplied number is "<<product<<" !"<<endl;
     
     //Exit stage right!
     return 0;
 }
+
 
